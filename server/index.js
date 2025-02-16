420e42c46e1b5baaee3b1d913597789ee7eb3b82
khushi Vashisth <khushidhiman951@gmail.com>
2025-02-16 14:16:15 +0530

    logo and search bar : adjusted

--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createPool({
-  host: "mysql-3db8a027-vishu-1019.a.aivencloud.com", // Aiven MySQL Host
-  port: 26486, // Port number
+  host: "mysql-1b5c0614-khushidhiman951.d.aivencloud.com", // Aiven MySQL Host
+  port: 13224, // Port number
   user: "avnadmin", // Username
   password: process.env.DB_PASSWORD, // Password
-  database: "studentData", // Database name
+  database: "defaultdb", // Database name
   // ssl: {
   //   // SSL mode required
   //   rejectUnauthorized: true,
@@ -30,8 +30,17 @@ app.get("/", (req, res) => {
 });
 
 app.post("/create", (req, res) => {
-  const info = "INSERT INTO user (`Firstname`,`Lastname`,`email`) VALUES(?)";
-  const VALUES = [req.body.Firstname, req.body.Lastname, req.body.email];
+  const info =
+    "INSERT INTO user (`Firstname`,`Lastname`,`email`,`phone`,`father`,`mother`,`address`) VALUES(?)";
+  const VALUES = [
+    req.body.Firstname,
+    req.body.Lastname,
+    req.body.email,
+    req.body.phone,
+    req.body.father,
+    req.body.mother,
+    req.body.address,
+  ];
   db.query(info, [VALUES], (err, data) => {
     if (err) return res.json(err);
     return res.json(data);
@@ -40,10 +49,19 @@ app.post("/create", (req, res) => {
 
 app.put("/update/:id", (req, res) => {
   const info =
-    "UPDATE user set `Firstname`=?, `Lastname`=? ,`email`=? WHERE `ID`=?";
-  const VALUES = [req.body.Firstname, req.body.Lastname, req.body.email];
+    "UPDATE user set `Firstname`=?, `Lastname`=? ,`email`=? `phone`=?, `father`=?, `mother`=?, `address`=? WHERE `id`=?";
+  const VALUES = [
+    req.body.Firstname,
+    req.body.Lastname,
+    req.body.email,
+    req.body.phone,
+    req.body.father,
+    req.body.mother,
+    req.body.address,
+  ];
 
   const id = req.params.id;
+  console.log("id", id);
 
   db.query(info, [...VALUES, id], (err, data) => {
     if (err) return res.json(err);
@@ -52,7 +70,7 @@ app.put("/update/:id", (req, res) => {
 });
 
 app.delete("/user/:id", (req, res) => {
-  const info = "DELETE FROM user WHERE ID=?";
+  const info = "DELETE FROM user WHERE id=?";
 
   const id = req.params.id;
 
