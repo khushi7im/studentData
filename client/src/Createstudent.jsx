a428c17f9c48d4823520bcc4399f28334dab234d
khushi Vashisth <khushidhiman951@gmail.com>
2025-02-16 14:19:51 +0530

    search bar & logo adjustment

--- client/src/Createstudent.jsx
+++ client/src/Createstudent.jsx
@@ -7,12 +7,24 @@ const Createstudent = () => {
   const navigate = useNavigate();
   const [Firstname, setfirstname] = useState("");
   const [Lastname, setlastname] = useState("");
+  const [father, setfathername] = useState("");
+  const [mother, setmothername] = useState("");
+  const [phone, setphone] = useState("");
+  const [address, setaddress] = useState("");
   const [email, setemail] = useState("");
 
   function HandleSubmit(event) {
     event.preventDefault();
     axios
-      .post("http://localhost:8082/create", { Firstname, Lastname, email })
+      .post("http://localhost:8082/create", {
+        Firstname,
+        Lastname,
+        email,
+        phone,
+        father,
+        mother,
+        address,
+      })
       .then((res) => {
         console.log(res);
         navigate("/");
@@ -21,39 +33,77 @@ const Createstudent = () => {
   }
 
   return (
-    <div className="wrapper">
+    <div className="wrapper2">
       <form action="" method="post" onSubmit={HandleSubmit}>
-        {" "}
         <h2>Add Student</h2>
+
         <div className="inputs">
           <label htmlFor="firstname">First Name :</label>
           <input
             onChange={(e) => setfirstname(e.target.value)}
             type="text"
             name="firstname"
-            placeholder="Enter First name"
+            placeholder="Enter First Name"
             required
           />
 
-          <label htmlFor="lastname">Last Name</label>
-
+          <label htmlFor="lastname">Last Name :</label>
           <input
             onChange={(e) => setlastname(e.target.value)}
             type="text"
             name="lastname"
-            placeholder="Enter Last name"
+            placeholder="Enter Last Name"
           />
 
-          <label htmlFor="email">Email </label>
+          <label htmlFor="fathername">Father's Name :</label>
+          <input
+            onChange={(e) => setfathername(e.target.value)}
+            type="text"
+            name="father"
+            placeholder="Enter Father's Name"
+            required
+          />
+
+          <label htmlFor="mothername">Mother's Name :</label>
+          <input
+            onChange={(e) => setmothername(e.target.value)}
+            type="text"
+            name="mother"
+            placeholder="Enter Mother's Name"
+            required
+          />
+
+          <label htmlFor="contact">Contact No :</label>
+          <input
+            onChange={(e) => setphone(e.target.value)}
+            type="text"
+            name="phone"
+            placeholder="Enter Contact Number"
+            required
+          />
+
+          <label htmlFor="address">Address :</label>
+          <input
+            onChange={(e) => setaddress(e.target.value)}
+            type="text"
+            name="address"
+            placeholder="Enter Address"
+            required
+          />
+
+          <label htmlFor="email">Email :</label>
           <input
             onChange={(e) => setemail(e.target.value)}
             type="email"
-            name="name"
+            name="email"
             placeholder="Enter Email"
             required
-          ></input>
+          />
         </div>
-        <button type="submit">Submit</button>
+
+        <button type="submit" className="button1">
+          Submit
+        </button>
       </form>
     </div>
   );
--- client/src/Student.jsx
+++ client/src/Student.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./main.css";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import logo from "./assets/images.png";
 
 const Student = () => {
   const [student, setStudent] = useState([]);
@@ -14,12 +15,12 @@ const Student = () => {
         .catch((err) => console.log(err));
     }
     datafetching();
-  });
+  }, []);
 
   const HandleDelete = async (id) => {
     try {
       await axios.delete("http://localhost:8082/user/" + id);
-      window.location.reload();
+      // window.location.reload();
     } catch (error) {
       console.log(error);
     }
@@ -27,32 +28,58 @@ const Student = () => {
 
   return (
     <div className="wrapper">
+      <header className="header">
+        <img src={logo} alt="Logo" className="logo" width="100px" />
+      </header>
       <div className="content">
+        {" "}
         <div className="addButton">
+          <div className="search">
+            <i class="fa-brands fa-searchengin"></i>
+            <p>Search</p>
+          </div>
           <Link to="/create">
             <button>+ Add </button>
           </Link>
         </div>
         <table>
           <thead>
-            <td>Fullname</td>
+            <td>Sr. No.</td>
+            <td>Firstname</td>
+            <td>Lastname</td>
+            <td>Father's name</td>
+            <td>Mother's name</td>
+            <td>contact no</td>
             <td>Email</td>
+            <td>Address</td>
             <td>Actions</td>
           </thead>
 
           {Array.isArray(student) &&
-            student.map((data) => (
+            student.map((data, index) => (
               <tr>
-                <td>
-                  {data.Firstname} {data.Lastname}
-                </td>
+                <td key="{data.id}">{index + 1}</td>
+                <td>{data.Firstname}</td>
+                <td>{data.Lastname}</td>
+                <td>{data.father}</td>
+                <td>{data.mother}</td>
+                <td>{data.phone}</td>
                 <td>{data.email}</td>
+                <td>{data.address}</td>
                 <td>
-                  <Link to={`update/${data.ID}`}>
-                    <button>Update</button>
+                  <Link to={`update/${data.id}`}>
+                    <button>
+                      <i class="fa-solid fa-pen"></i>
+                    </button>
                   </Link>
-
-                  <button onClick={(e) => HandleDelete(data.ID)}>Delete</button>
+                  <button
+                    onClick={(e) => {
+                      HandleDelete(data.id);
+                      window.location.reload(); // Reload after function execution
+                    }}
+                  >
+                    <i className="fa-solid fa-trash"></i>
+                  </button>
                 </td>
               </tr>
             ))}
